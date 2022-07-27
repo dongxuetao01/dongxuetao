d72a2d31a3403b20707e282b4643e9f431745bf4
wangwei138 <wangwei138@baidu.com>
2022-07-27 21:01:43 +0800

    first Submit

---
+++ cc.c
@@ -0,0 +1,14 @@
+// File Name: cc.c
+// Author: xiaxiaosheng
+// Created Time: 2022年07月27日 星期三 20时45分28秒
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+int main(){
+
+return 0 ;
+}
